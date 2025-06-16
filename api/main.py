040474752b025cff9529d543f21ce11fe1cf1407
tonybnya <nya.tony2010@gmail.com>
2025-06-16 11:42:30 +0100

    feat: handle main FastAPI app

--- api/main.py
+++ api/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello from api!")
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from .crud import get_random_puzzle
+from .database import SessionLocal, engine
+from .models import Base
+
+app = FastAPI(title="Sudoku API")
+
+# Create tables
+Base.metadata.create_all(bind=engine)
 
 
-if __name__ == "__main__":
-    main()
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/puzzle")
+def read_random_puzzle(db: Session = Depends(get_db)):
+    puzzle = get_random_puzzle(db)
+    if puzzle:
+        return {"puzzle": puzzle.puzzle}
+    return {"error": "No puzzles found."}
