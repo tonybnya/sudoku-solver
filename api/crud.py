291c5164b44d8262ed914a0b266199142f331d89
tonybnya <nya.tony2010@gmail.com>
2025-06-16 11:40:24 +0100

    feat: add db logic

---
+++ api/crud.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Session
+from sqlalchemy.sql import func
+
+from .models import Puzzle
+
+
+def get_random_puzzle(db: Session):
+    return db.query(Puzzle).order_by(func.random()).first()
